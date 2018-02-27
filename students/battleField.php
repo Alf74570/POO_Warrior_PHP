d968166f198d06dc0e5e8b0d2af92a39e1541564
Alexandre <a.biagi@orange.fr>
2018-02-27 13:16:36 +0100

    Les 18 points du TP terminés

--- students/battleField.php
+++ students/battleField.php
@@ -6,6 +6,27 @@
 
   class BattleField extends BaseBattleField
   {
+    static function createMyWarrior(){
+      $guerrier = new MarvelWarrior($GLOBALS['warriorID']);
+      $guerrier->setImageUrl("https://img00.deviantart.net/b4c8/i/2012/293/d/b/harsk_by_shiprock-d5iex0r.jpg");
 
+      $weapon = new Weapon("Hache", 50);
+      $weapon->setImageUrl("https://www.no-cookie.com/w/images/1/18/Hache_de_combat_de_Victo-screen.jpg");
+    
+      $guerrier->setWeapon($weapon);
 
+      $guerrier->saveNew();
+    }
+
+    static function createOtherWarrior(){
+      $guerrier = new PokemonWarrior("Pokemon");
+      $guerrier->setImageUrl("http://www2.mes-coloriages-preferes.biz/colorino/Images/Large/Personnages-celebres-Nintendo-Pokemon-509301.png");
+
+      $weapon = new Weapon("Feu", 30);
+      $weapon->setImageUrl("https://desarmons.net/wp-content/uploads/2016/04/BT-31000-A_2_big.jpeg");
+
+      $guerrier->setWeapon($weapon);
+
+      $guerrier->saveNew();
+    }
   }
--- students/warrior.php
+++ students/warrior.php
@@ -3,9 +3,112 @@
 require_once __DIR__ . "/../base/localWarrior.php";
 
 
-$GLOBALS['warriorID'] = 'azertyuiop';
+$GLOBALS['warriorID'] = 'azerty';
 
 // Définissez vos class de combattants
 abstract class Warrior extends LocalWarrior {
+    public $id;
+    public $name;
+    public $speed;
+    public $life;
+    public $shield;
+    public $imageUrl;
+    public $weapon;
 
-};
+    //Créer Le Constructeur
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20){
+        
+        //Le constructeur initialise l'id, speed, life et shield
+        $this->id = $id;
+        $this->speed = $speed;
+        $this->life = $life;
+        $this->shield = $shield;
+    }
+
+    //Créer méthode setWeapon avec weapon en argument --SETTER--
+    public function setWeapon(Weapon $weapon){
+        $this->weapon = $weapon;
+    }
+
+    //Créer méthode setImageURL
+    public function setImageUrl($url){
+        $this->imageUrl = $url;
+    }
+
+}
+
+// Créer Les Classes StartrekWarrior, MarvelWarrior et PokemonWarrior
+class StartrekWarrior extends Warrior
+{
+    public $mentalPower;
+    
+    //Créer Le Constructeur
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20, $mentalPower = 8){
+        
+        //Le constructeur herite du constructeur Warrior et affecte $mentalPower
+        parent::__construct($id, $speed, $life, $shield);
+        $this->mentalPower = $mentalPower;
+    }
+    
+    //Créer une methode power qui retourne l'attribut
+    public function power(){
+        return $this->mentalPower;
+    }
+
+}
+
+class MarvelWarrior extends Warrior
+{
+    public $superPower;
+
+    //Créer Le Constructeur
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20, $superPower = 100){
+       
+        //Le constructeur herite du constructeur Warrior et affecte $mentalPower
+        parent::__construct($id, $speed, $life, $shield);
+        $this->superPower = $superPower;
+
+    }
+
+    //Créer une methode power qui retourne l'attribut
+    public function power(){
+        return $this->superPower;
+    }
+}
+
+class PokemonWarrior extends Warrior 
+{
+    public $level;
+
+    //Créer Le Constructeur
+    public function __construct($id, $speed = 30, $life = 100, $shield = 20, $level = 1){
+        
+        //Le constructeur herite du constructeur Warrior et affecte $mentalPower
+        parent::__construct($id, $speed, $life, $shield);
+        $this->level = $level;
+    }
+    
+    //Créer une methode power qui retourne l'attribut
+    public function power(){
+        return $this->level;
+    }
+}
+
+//Créer une classe weapon
+class Weapon extends Warrior
+{
+    public $id;
+    public $strength;
+    public $imageUrl;
+
+    public function __construct($id, $strength){
+        $this->id = $id;
+        $this->strength = $strength;
+    }
+
+    //Créer méthode setImageURL
+    public function setImageUrl($url){
+        $this->imageUrl = $url;
+    }
+}
+
